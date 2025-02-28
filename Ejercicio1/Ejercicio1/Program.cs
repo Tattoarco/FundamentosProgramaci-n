3b3a5859186ce97c487f411711457b12e06fa8b4
tattoarco <atatianamosquera@gmail.com>
2025-02-28 13:11:15 -0500

    Primer commit

---
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresar el primer número");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresar el segundo número");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresar el segundo número");
+            int num3 = int.Parse(Console.ReadLine());
+
+            if (num1 != num2 && num1 != num3)
+            {
+                if (num2 != num1 && num2 != num3)
+                {
+                    if(num3 != num1 && num3 != num2)
+                    {
+                        if (num1 > num2 && num1 > num3)
+                        {
+                            Console.WriteLine("El número más grande es: " + num1);
+                        }else if (num2 > num1 && num2 > num3)
+                        {
+                            Console.WriteLine("El número más grande es: " + num2);
+                        }else if(num3 > num1 && num3 > num2)
+                        {
+                            Console.WriteLine("El número más grande es: " + num3);
+                        }
+
+                        int[] numeros = { num1, num2, num3 };
+
+                        Array.Sort(numeros);
+
+                        Console.WriteLine("Números en orden ascendente:");
+                        foreach (int num in numeros)
+                        {
+                            Console.WriteLine(num);
+                        }
+                    }
+                }
+            }else
+            {
+                Console.WriteLine("Ingresar números diferentes");
+            }
+        }
+    }
+}
