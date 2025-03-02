641ba71cf3116b9e907cbb482ad915a8e88c7986
Tattoarco <atatianamosquera@gmail.com>
2025-03-01 19:57:34 -0500

    Ejercicio de if (invensible)

---
+++ Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("¿El personaje de juego pudedisparar?");
+            Console.WriteLine("Ingresa true para hacer invencible al personaje o false para lo contrario");
+            string invensible = Console.ReadLine();
+
+            int numero;
+            Random rnd = new Random();
+            numero = rnd.Next(1, 6);//Devuelve un número entre 0 y 5 
+
+            if (invensible == "true" && numero > 0)
+            {
+                Console.WriteLine("Tienes: "+numero+" municiones");
+                Console.WriteLine("El personaje está disparando");
+            }
+        }
+    }
+}
